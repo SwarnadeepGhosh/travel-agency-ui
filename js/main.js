a9ae2e015318ef4d522981b7376ae6aa1c5e88df
Swarnadeep Ghosh <swarna.lyf@gmail.com>
2021-06-14 13:38:57 +0530

    Completed hotel page

--- js/main.js
+++ js/main.js
@@ -31,3 +31,88 @@ $(document).ready(function(){
 
 
 });
+
+function openpop() {
+  $("#popmessg").slideToggle(500);
+  $(".transprntbgpop").show();
+
+}
+
+function closetrnsblc() {
+  $("#popmessg").slideToggle(500);
+  $(".transprntbgpop").hide();
+
+}
+
+$(document).ready(function () {
+  $("#flip").click(function () {
+    $("#panel").slideToggle("slow");
+  });
+});
+
+$(document).ready(function () {
+
+
+  $('#saveCategories').click(function () {
+    $(".input_fields_wrap").each(function () {
+      var sum = 0;
+      var kids = 0;
+      $(this).find('span').each(function () {
+        sum += +$(this).find('.useronename').val();
+        kids += +$(this).find('.useronephone').val();
+
+      })
+      var totaloffqty = $('#totaloffqty').val();
+      $('#actualresult').val('Rooms:' + totaloffqty + ', Adults:' + sum + ', Child:' + kids);
+    })
+  });
+
+
+
+  $('#patbbtn').trigger('click');
+
+
+  var max_fields = 10; //maximum input boxes allowed
+  var wrapper = $(".input_fields_wrap"); //Fields wrapper
+  var add_button = $(".add_field_button"); //Add button ID
+
+
+  //var addingprice = 0;
+  var x = 1; //initlal text box count
+  $(add_button).click(function (e) { //on add input button click
+    //alert(offerqty);
+    var offerqty = $('#totaloffqty').val();
+    $('#totaloffqty').val(parseFloat(offerqty) + parseFloat(1));
+
+    e.preventDefault();
+    if (x < max_fields) { //max input box allowed
+      x++; //text box increment
+      $(wrapper).append('<span><select class="form-control useronename"><option value="">-- Adult --</option><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option></select><select  class="form-control useronephone" ><option value="">-- Child --</option><option value="0">0</option><option value="1">1</option><option value="2">2</option></select><a href="javascript:void();" class="remove_field rmvbtn"><i class="far fa-times-circle"></i></a><div class="clearfix"></div></span>'); //add input box
+    }
+  });
+
+  $(wrapper).on("click", ".remove_field", function (e) { //user click on remove text
+    var offerqty = $('#totaloffqty').val();
+    $('#totaloffqty').val(parseFloat(offerqty) - parseFloat(1));
+
+    e.preventDefault(); $(this).parent('span').remove(); x--;
+  })
+
+
+});
+
+$(document).ready(function() {
+  $('li.active').removeClass('active');
+  $('a[href="' + location.pathname + '"]').closest('li').addClass('active'); 
+});
+
+
+/********************* Search block in hotel starts *****************************************************/
+flatpickr(".datepicker",
+      {});
+
+      const choices = new Choices('[data-trigger]',
+      {
+        searchEnabled: false,
+        itemSelectText: '',
+      });
